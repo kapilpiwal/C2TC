840b1bbea58751672e1063100cb4aee62d00a0e9
kapilpiwal <kapilpiwal99@gmail.com>
2022-03-07 15:21:18 +0530

    ifelsedemo

---
+++ m11batch/src/m11batch/If_else_Demo.java
@@ -0,0 +1,21 @@
+package m11batch;
+
+public class If_else_Demo {
+
+	public static void main(String[] args) {
+		/*boolean x=false;
+		if(x=true)
+			System.out.println("GM");
+		else
+			System.out.println("GN"); 
+	
+		if(true)
+			{int x=20;}*/
+		if(true)
+			if(true) //nearest if statement
+				System.out.println("good morning");
+			else
+				System.out.println("good noon");
+	}
+
+}
